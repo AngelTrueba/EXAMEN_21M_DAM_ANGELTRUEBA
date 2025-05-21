5699fcb7735a52c6a65e2b2eaf3846be6784a6f8
AngelTrueba <130221797+AngelTrueba@users.noreply.github.com>
2025-05-21 11:26:37 +0200

    Primer commit

---
+++ src/paqueteMaestro/Main.java
@@ -0,0 +1,23 @@
+package paqueteMaestro;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Buenos días Ceinmark, soy Angel Trueba");
+		
+		System.out.println("Introduce un número para averiguar si es primo o no:");
+		
+		int p = Integer.parseInt(sc.nextLine());
+		
+		if ((p%2) == 0) {
+			System.out.println("El numero es prime");
+		} else {
+			System.out.println("El numero no es primo");
+		}
+	}
+}
